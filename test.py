e5f7b3b91e7c06ad6ec6d9b316216eec8888566f
garyyys <allgarryyys@gmail.com>
2022-05-03 13:56:56 +0300

    about metaclasses

--- test.py
+++ test.py
@@ -1,28 +1,12 @@
-class ExceptionPrintData(Exception):
-    """Класс исключение при отправке данных принтеру"""
-
-    def __int__(self, *args):
-        self.message = args[0] if args else None
-
-    def __str__(self):
-        return f'Ошибка: {self.message}'
+def create_class_point(name, base, attrs): #<- простейший метакласс с помощью функции
+    attrs.update({'MAX_COORD': 100, 'MIN_COORD': 0}) #<- задает атрибуты поумолчанию
+    return type(name, base, attrs)
 
 
-class PrintData:
-    def print(self, data):
-        self.send_data(data)
-        print(f'печать: {str(data)}')
+class Point(metaclass=create_class_point):
+    def get_coord(self):
+        return (0, 0)
 
-    def send_data(self, data):
-        if not self.send_data_to_print(data):
-            raise ExceptionPrintData('Принтер не отвечает')
-
-    def send_data_to_print(self, data):
-        return False
-
-p = PrintData()
-p.print('123')
-# try:
-#     p.print('123')
-# except Exception:
-#     print('Принтер не отвечает')
+pt = Point()
+print(pt.MAX_COORD)
+print(pt.get_coord())
